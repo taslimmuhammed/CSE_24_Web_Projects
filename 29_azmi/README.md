2a829609c9b7ce1c152f6c33f9ff09e8943aaeac
LowerCaseL084 <127769914+LowerCaseL084@users.noreply.github.com>
2023-10-09 06:40:03 +0530

    Azmi, Sreejith, Rahul

--- 29_azmi/README.md
+++ 29_azmi/README.md
@@ -11,9 +11,9 @@ A simple web application which will generate a QR Code.
 ![CSS](https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white)
 ![JS](https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
 
-## API Used
+## Plugin Used
 
-Visit Website: https://goqr.me/api/
+Visit Website: https://davidshimjs.github.io/qrcodejs/
 
 ## How to use Project
 
--- 29_azmi/script.js
+++ 29_azmi/script.js
@@ -1,15 +1,14 @@
 const btn = document.querySelector('.cs29-btn');
 const code = document.querySelector('.cs29-code');
 const input = document.querySelector('.cs29-input');
+const qr = document.querySelector('#qrcode');
+
+const qrcode = new QRCode("qrcode");
 
 btn.addEventListener('click', generate);
 
 function generate() {
 	const data = input.value;
-	const URL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
-	code.src = URL;
-	/*setTimeout(() => {
-		alert("Successfully Generated QR");
-	}, 400);*/
+	qrcode.makeCode(data);
 }
 
