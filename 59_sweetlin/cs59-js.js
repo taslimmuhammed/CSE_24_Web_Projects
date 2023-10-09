eaf41d0d8446081ffa10f1f988f3cdc8152e9ae7
taslimmuhammed <taslimmuhammed67@gmail.com>
2023-10-09 23:58:34 +0530

    59

--- 59_sweetlin/cs59-js.js
+++ 59_sweetlin/cs59-js.js
@@ -1,12 +1,11 @@
 const container = document.getElementById('container')
-const colors = ['#e74c3c','#8e44ad','#3498db','#e67e22','#2ecc71']
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 const SQUARES = 504
-for(let i=0;i<SQUARES;i++)
-{
+for (let i = 0; i < SQUARES; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
 
-  square.addEventListener('mouseover',()=> ReadableStreamDefaultController(square))
-  square.addEventListener('mouseover',()=>ReadableStreamDefaultController(square))
+  square.addEventListener('mouseover', () => ReadableStreamDefaultController(square))
+  square.addEventListener('mouseover', () => ReadableStreamDefaultController(square))
   container.appendChild(square)
 }
